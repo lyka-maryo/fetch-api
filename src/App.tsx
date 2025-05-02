57e45c57638107f5be6dd9b7ed7a062937d48c94
Lyka <bin.lyka@institute.pse.ngo>
2025-05-02 14:47:30 +0700

    add again

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
 const App = () => {
   return (
     <>
-      <div className="bg-gray-200 text-blue-700 text-3xl">Hello</div>;
+      <nav className="bg-gray-50 dark:bg-gray-700 ">
+        <div className="max-w-screen-xl px-4 py-3 mx-auto">
+          <div className="flex items-center ">
+            <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-900 dark:text-white hover:underline text-2xl"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-900 dark:text-white hover:underline text-2xl"
+                >
+                  Company
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-900 dark:text-white hover:underline text-2xl"
+                >
+                  Team
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-900 dark:text-white hover:underline text-2xl"
+                >
+                  Features
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
